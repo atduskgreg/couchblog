3569ecff56a631e5072625adab8e819fed7d811d
Greg Borenstein <greg@mfdz.com>
2008-07-18 15:55:52 -0700

    committing to revert

--- app/controllers/posts/index.js
+++ app/controllers/posts/index.js
@@ -5,48 +5,11 @@ function(request, db){
 	
 	if(request.cookie.session) authenticateSession(sessionFromCookie(request.cookie));
 	var currentUser = currentUser(request);
-	
-	var body = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"><html>';
-	body += '<head><title>CouchDB PDX</title>';
-	body += '<script src="/_utils/script/jquery.js"></script>';
-	body += '<script src="/pdxblog/public/main.js"></script>';
-
-	body += '<link rel="stylesheet" type="text/css" charset="utf-8" href="/pdxblog/public/main.css" /></head><body>';
-	body += '<div id="body"><div id="header"><img src="/pdxblog/public/couchdb-pdx-header.png" /><ul id="nav"><li><a href="/pdxblog/_action/posts/index">home</a></li><li><a href="http://groups.google.com/group/couchdb-pdx/">mailing list</a></li>';
-	              
-	if (currentUser){
-	body += '<li>logged in as: ' + currentUser + '<li>';
-	body += '<li><a href="/pdxblog/_action/posts/new">write post</a></li>';
-	body += '<li><a href="/pdxblog/_action/account/new">add user</a></li>';
-	body += '<li><a href="/pdxblog/_action/account/logout">logout</a></li></ul>';
-	} else {
-		body += '<li><a href="/pdxblog/_action/account/login" id="login-link">login</a></li><li><form id="login" action="/pdxblog/_action/account/login" method="post">                                   ';
-  	body += '  <label>Username</label>                                                                   ';
-  	body += '  <input name="login"></input>                                                            ';
-  	body += '                                                                                               ';
-  	body += '  <label>Password</label>                                                                   ';
-  	body += '  <input name="password" type="password"></input>                                          ';
-  	body += '                                                                                            ';
-  	body += '  <label class="submit">&nbsp;</label><input class="submit" type="submit" value="submit" />    ';
-  	body += '                                                                                           ';
-  	body += '</form> </li></ul>                                                                                      ';
-	}                                                              
-
-	                                                                                                        
-	body += '</div>'	;
-	body += '<div id="main">'
-	body += '<ul>'
-	// get all the posts
 	var doc = db.view("posts/posts-map");
 	
-	doc.rows.reverse().forEach(function(row){
-  	var post = row.value;
 
 
-		body += '<li><h2>'+ post.title+'</h2>';
-
-		body += '<p class="attribution">'
-				if (currentUser){
+		if (currentUser){
     	body += '<a class="edit" href="/pdxblog/_action/posts/edit?_id='+post._id+'">edit</a>';
 		};
 		
